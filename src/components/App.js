c4d52a67f2b5e10a9af1842e306df339af1a9970
Zaur Bairamau <bairamau.z@gmail.com>
2018-12-28 16:27:07 +0300

    add Team's page component

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ import Home from './Home'
 import Players from './Players'
 import Teams from './Teams'
 import Navbar from './Navbar'
-
+import TeamPage from './TeamPage'
 class App extends Component {
   render() {
     return (
@@ -17,8 +17,9 @@ class App extends Component {
           <Navbar />
           <Switch>
             <Route exact path='/' component={Home} />
-            <Route path='/players' component={Players} />
+            <Route  exact path='/players' component={Players} />
             <Route path='/teams' component={Teams} />
+            <Route path='/:teamId' exact component={TeamPage}/>
             <Route render={()=><h1 className='text-center'>404</h1>} />
           </Switch>
         </div>
