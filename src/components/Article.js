3b99711797d307da872eb793d3419ac492fd1288
Zaur Bairamau <bairamau.z@gmail.com>
2018-12-28 17:20:19 +0300

    add Article component

---
+++ src/components/Article.js
@@ -0,0 +1,46 @@
+import { Component } from 'react'
+import PropTypes from 'prop-types'
+import { getArticle } from '../api'
+
+class Article extends Component {
+
+  static propTypes = {
+    teamId: PropTypes.string.isRequired,
+    articleId: PropTypes.string.isRequired,
+    children: PropTypes.func.isRequired,
+  }
+
+  state = {
+    article: null
+  }
+
+  componentDidMount() {
+    const { teamId, articleId } = this.props
+    this.fetchArticle(teamId, articleId)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.articleId !== nextProps.articleId) {
+      this.fetchArticle(nextProps.teamId, nextProps.articleId)
+    }
+  }
+
+  fetchArticle = (teamId, articleId) => {
+    this.setState(() => ({
+      article: null,
+    }))
+
+    getArticle(teamId, articleId)
+      .then((article) => this.setState(() => ({
+        article
+      })))
+  }
+
+  render() {
+    return (
+      this.props.children(this.state.article)
+    )
+  }
+}
+
+export default Article
