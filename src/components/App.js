e4eca008a062cf4544e216c8e4317d5a28f0dd47
Zaur Bairamau <bairamau.z@gmail.com>
2019-01-02 22:01:37 +0300

    add code splittig

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,56 @@ import {
   Switch,
   BrowserRouter as Router,
 } from 'react-router-dom'
-import Home from './Home'
-import Players from './Players'
-import Teams from './Teams'
+
+import DynamicImport from './DynamicImport'
+import Loading from './Loading'
 import Navbar from './Navbar'
-import TeamPage from './TeamPage'
-import Articles from './Articles'
+
+
+const Home = (props) => (
+  <DynamicImport load={() => import('./Home')}>
+    {(Component) => Component === null
+      ? <Loading />
+      : <Component {...props} />
+    }
+  </DynamicImport>
+)
+
+const Players = (props) => (
+  <DynamicImport load={() => import('./Players')}>
+    {(Component) => Component === null
+      ? <Loading />
+      : <Component {...props} />
+    }
+  </DynamicImport>
+)
+
+const Teams = (props) => (
+  <DynamicImport load={() => import('./Teams')}>
+    {(Component) => Component === null
+      ? <Loading />
+      : <Component {...props} />
+    }
+  </DynamicImport>
+)
+
+const TeamPage = (props) => (
+  <DynamicImport load={() => import('./TeamPage')}>
+    {(Component) => Component === null
+      ? <Loading />
+      : <Component {...props} />
+    }
+  </DynamicImport>
+)
+
+const Articles = (props) => (
+  <DynamicImport load={() => import('./Articles')}>
+    {(Component) => Component === null
+      ? <Loading />
+      : <Component {...props} />
+    }
+  </DynamicImport>
+)
 
 class App extends Component {
   render() {
@@ -21,9 +65,9 @@ class App extends Component {
             <Route exact path='/' component={Home} />
             <Route path='/players' component={Players} />
             <Route path='/teams' component={Teams} />
-            <Route path='/:teamId' exact component={TeamPage}/>
-            <Route path='/:teamId/articles'component={Articles}/>
-            <Route render={()=><h1 className='text-center'>404</h1>} />
+            <Route path='/:teamId' exact component={TeamPage} />
+            <Route path='/:teamId/articles' component={Articles} />
+            <Route render={() => <h1 className='text-center'>404</h1>} />
           </Switch>
         </div>
       </Router>
---
+++ src/components/DynamicImport.js
@@ -0,0 +1,20 @@
+import { Component } from 'react'
+
+
+class DynamicImport extends Component {
+
+  state = {
+    component: null
+  }
+
+  componentDidMount() {
+    this.props.load()
+      .then((module) => this.setState(() => ({ component: module.default })))
+  }
+
+  render() {
+    return this.props.children(this.state.component)
+  }
+}
+
+export default DynamicImport
