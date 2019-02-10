d686df94ee782b7ebbc57f08aa6c8d63bb0b3af4
Zaur Bairamau <bairamau.z@gmail.com>
2019-02-10 15:27:44 +0300

    Updates

---
+++ precache-manifest.6c84cb3ac063009b26c81ea7c0757b59.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "759648dd1b73054b6664",
+    "url": "/basketball-league/static/js/5.759648dd.chunk.js"
+  },
+  {
+    "revision": "0878248bcf0c3a330177",
+    "url": "/basketball-league/static/js/0.0878248b.chunk.js"
+  },
+  {
+    "revision": "8dc52e85def9a3d0d4d7",
+    "url": "/basketball-league/static/js/runtime~main.8dc52e85.js"
+  },
+  {
+    "revision": "2efed24e86c25a5b53c4",
+    "url": "/basketball-league/static/js/main.2efed24e.chunk.js"
+  },
+  {
+    "revision": "5453f803f17d347a4bb4",
+    "url": "/basketball-league/static/js/3.5453f803.chunk.js"
+  },
+  {
+    "revision": "2105b4d74dac24bbf072",
+    "url": "/basketball-league/static/js/4.2105b4d7.chunk.js"
+  },
+  {
+    "revision": "464ceb26bae0c0ecaa5e",
+    "url": "/basketball-league/static/js/1.464ceb26.chunk.js"
+  },
+  {
+    "revision": "f9c0eb562aadd48ece9f",
+    "url": "/basketball-league/static/js/6.f9c0eb56.chunk.js"
+  },
+  {
+    "revision": "21d415cd82c349d87cb0",
+    "url": "/basketball-league/static/js/7.21d415cd.chunk.js"
+  },
+  {
+    "revision": "69ed453ca0f1df3e8285",
+    "url": "/basketball-league/static/js/8.69ed453c.chunk.js"
+  },
+  {
+    "revision": "11f5acfeaff85c95e9b6",
+    "url": "/basketball-league/static/js/9.11f5acfe.chunk.js"
+  },
+  {
+    "revision": "2efed24e86c25a5b53c4",
+    "url": "/basketball-league/static/css/main.6927cef1.chunk.css"
+  },
+  {
+    "revision": "cc422720af49afe2647f7fc7d5c5640f",
+    "url": "/basketball-league/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/basketball-league/precache-manifest.6c84cb3ac063009b26c81ea7c0757b59.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/basketball-league/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
